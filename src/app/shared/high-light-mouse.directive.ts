744b5f512c6eeb0df819d4b281c4587c6998b0f7
Leonardo Queres <leonardoqueres@hotmail.com>
2019-08-28 18:10:35 -0300

    HostListener and HostBinding
    Diretiva com HostListener e HostBinding

--- src/app/shared/high-light-mouse.directive.ts
+++ src/app/shared/high-light-mouse.directive.ts
@@ -1,19 +1,33 @@
-import { Directive, HostListener, ElementRef, Renderer } from '@angular/core';
+import { Directive, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[HighLightMouse]'
 })
 export class HighLightMouseDirective {
-  @HostListener('mouseLeave') onmouse(){
-    this._renderer.setElementStyle(this._elementRef.nativeElement, 'backgroundColor', 'white');
-  };
-  @HostListener('mouseenter') onmouseover(){
-    this._renderer.setElementStyle(this._elementRef.nativeElement, 'backgroundColor', 'yellow');
-  };
+  
+    possibleColors = [
+      'darksalmon', 'hotpink', 'lightskyblue', 'goldenrod', 'peachpuff',
+      'mediumspringgreen', 'cornflowerblue', 'blanchedalmond', 'lightslategrey'
+    ];
 
- 
+    @HostBinding('style.color') color: string;
+    @HostBinding('style.backgroundColor') borderColor: string;
 
-  constructor(private _elementRef: ElementRef,
-    private _renderer: Renderer) { }
+    @HostListener('mouseleave') mouseLeave(){
+      //this._renderer.setElementStyle(this._elementRef.nativeElement, 'backgroundColor', 'white');
+      const colorPick = Math.floor(Math.random() * this.possibleColors.length);
+      this.color = 'black';
+      this.borderColor = this.possibleColors[colorPick];
+    };
+    @HostListener('mouseenter') onmouseover(){
+      //this._renderer.setElementStyle(this._elementRef.nativeElement, 'backgroundColor', 'yellow');
+      const colorPick = Math.floor(Math.random() * this.possibleColors.length);
+      this.color = 'black';
+      this.borderColor = this.possibleColors[colorPick];
+    };
+
+    constructor() { }
+  // private _elementRef: ElementRef,
+  //   private _renderer: Renderer
 
 }
